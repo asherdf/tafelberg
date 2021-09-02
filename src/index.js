85981f09fc7bb81e807c24d67e1ccce73677197c
Sam Friedman <samtx@outlook.com>
2021-09-02 11:11:45 -0500

    Remove javascript to dynamically create numbered options for guest count select input. Add the values statically to the html

--- src/index.js
+++ src/index.js
@@ -184,21 +184,6 @@ document.querySelector("#dates-and-guests").addEventListener("submit", async(eve
     let daysBetween = getDaysBetween(new Date(arriveDate), new Date(departDate));
     console.log("Duration:  " + diffInDays);
     }
-);
-
-
-// 1)   Grab the correct element by its ID
-// 1.1)   Create a new element that will be our child element
-// 2)   Insert the child element into the parent
-// 3)   Insert the guest number into child element
-// 4)   Increase the number in the child element
-// 5)   Append the list of child elements 30 times
-
-for (let i=1; i<=30; i++) {
-    const guestOption = document.getElementById("select-number-of-guests")
-    const newOption = document.createElement("option");
-    newOption.innerHTML = i;
-    guestOption.appendChild(newOption);
 }
 
 const dateSubmitButton = document.querySelector("#dates-and-guests");
